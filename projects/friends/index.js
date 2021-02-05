35f2a8a9276a243b0b5f59a5d4b7568acecc695d
ilyayak <ilya.yakusheff@yandex.ru>
2021-02-05 23:27:32 +0700

    commit

---
+++ projects/friends/index.js
@@ -0,0 +1,7 @@
+import './index.html';
+import FriendsFilter from './friendsFilter';
+
+new FriendsFilter(
+  document.querySelector('.list.all-friends'),
+  document.querySelector('.list.best-friends')
+);
